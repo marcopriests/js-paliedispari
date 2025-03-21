946d327ddd3c90fbaa8cc77819c111407403405c
marcopriests <prtmrc98@gmail.com>
2025-03-21 15:26:15 +0100

    [UPD] Pari e Dispari - funzione randomNum() fatta

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
-//PALINDROMA
-function isPalindrome(string) {
+// //PALINDROMA
+// function isPalindrome(string) {
 
-    let reverseWord = ""
+//     let reverseWord = ""
 
-    for (let i = string.length - 1; i >= 0; i--) {
+//     for (let i = string.length - 1; i >= 0; i--) {
         
-        reverseWord += string[i]
+//         reverseWord += string[i]
 
-    }
+//     }
 
-    if (string.toLowerCase() == reverseWord.toLowerCase()) {
+//     if (string.toLowerCase() == reverseWord.toLowerCase()) {
         
-        return "SI, la parola " + string.toLowerCase() + " risulta essere palindroma"
+//         return "SI, la parola " + string.toLowerCase() + " risulta essere palindroma"
 
-    } else {
+//     } else {
 
-        return "la parola " + string.toLowerCase() + " NON risulta essere palindroma"
+//         return "la parola " + string.toLowerCase() + " NON risulta essere palindroma"
 
-    }
+//     }
 
 
+// }
+
+// let userWord = prompt("Inserisci la parola che vuoi controllare")
+
+// console.log(isPalindrome(userWord))
+
+// //PARI E DISPARI
+function randomNum() {
+    let result = Math.floor(Math.random() * 5 + 1)
+
+    return result
 }
 
-let userWord = prompt("Inserisci la parola che vuoi controllare")
-
-console.log(isPalindrome(userWord))
-
-//PARI E DISPARI
+console.log(randomNum())
